f6c3dfcfb000f5229c4bd5cfc826024a1137d226
hazel-ys-lin <hazel.ys.lin@gmail.com>
2023-02-21 09:56:15 +0800

    [Feature] psqltos3 operator

---
+++ dags/datapipeline_dag.py
@@ -0,0 +1,36 @@
+"""
+    DAG to set the workflow of
+    processing data
+"""
+
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator import PythonOperator
+from psqltos3_operator import psqlToS3Operator
+
+with DAG(
+        "psqlToS3",
+        default_args={
+            "depends_on_past": True,
+            "email": ["hazel.ys.lin@viewsonic.com"],
+            "email_on_failure": False,
+            "email_on_retry": False,
+            "retries": 1,
+            "retry_delay": timedelta(minutes=5),
+        },
+        description="PostgresQL to S3",
+        # scehdule=timedelta(days=1),
+        schedule_interval='@daily',
+        start_date=datetime(2023, 2, 21),
+        catchup=False,
+        tags=["dataPipeline"],
+) as dag:
+    export_task = psqlToS3Operator(
+        task_id="psqltos3",
+        postgres_conn_id="uvs_postgres_conn",
+        s3_conn_id="",
+        sql_query="SELECT * FROM user_org;",
+        s3_bucket="",
+        s3_key="",
+    )
---
+++ operators/psqltos3_operator.py
@@ -0,0 +1,46 @@
+"""
+    Operator to export Postgres tables
+    and upload them to S3 bucket
+"""
+
+import csv
+import io
+import os
+
+from airflow.models import BaseOperator
+from airflow.utils.decorators import apply_defaults
+from airflow.hooks.postgres_hook import PostgresHook
+from airflow.hooks.S3_hook import S3Hook
+
+
+class psqlToS3Operator(BaseOperator):
+    """
+        Export data from psql and upload it to s3
+    """
+
+    @apply_defaults
+    def __init__(self, postgres_conn_id: str, s3_conn_id: str, sql_query: str, s3_bucket: str,
+                 s3_key: str, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.postgres_conn_id = postgres_conn_id
+        self.s3_conn_id = s3_conn_id
+        self.sql_query = sql_query
+        self.s3_bucket = s3_bucket
+        self.s3_key = s3_key
+
+    def execute(self, context):
+        postgres_hook = PostgresHook(postgres_conn_id=self._postgres_conn_id)
+        s3_hook = S3Hook(aws_conn_id=self._s3_conn_id)
+
+        results = postgres_hook.get_records(self.sql_query)
+
+        data_buffer = io.StringIO()
+        csv_writer = csv.writer(data_buffer, lineterminator=os.linesep)
+        csv_writer.writerows(results)
+        data_buffer_binary = io.BytesIO(data_buffer.getvalue().encode())
+        s3_hook.load_file_obj(
+            file_obj=data_buffer_binary,
+            bucket_name=self._s3_bucket,
+            key=self._s3_key,
+            replace=True,
+        )
